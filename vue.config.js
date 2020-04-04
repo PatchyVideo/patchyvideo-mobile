5c338cf1b6d88992231861bd49209491682f384a
wrzrmzx <1510033076@qq.com>
2020-04-04 11:30:53 +0800

    设置代理

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,42 @@
-
 const webpack = require('webpack')
 const path = require("path");
 module.exports = {
- 
-publicPath: './',
-outputDir: 'dist',
+
+    publicPath: './',
+    outputDir: 'dist',
     configureWebpack: {
         plugins: [
             new webpack.ProvidePlugin({
                 jQuery: 'jquery',
                 $: 'jquery',
-                'windows.jQuery':'jquery'
+                'windows.jQuery': 'jquery'
             })
         ]
+    },
+    devServer: {
+        proxy: {
+            '/be/': {
+                target: "https://thvideo.tv/be/",
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/be/': ''
+                }
+            },
+            '/images/': {
+                target: "https://thvideo.tv/images/",
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/images/': ''
+                }
+            },
+            '/autocomplete/ ': {
+                target: "https://thvideo.tv/autocomplete/",
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/autocomplete/': ''
+                }
+            }
+        }
+
     }
 }
